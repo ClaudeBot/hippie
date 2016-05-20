6c643cc32a53e09d9b078b95ba385c42d698b038
Ian Lai <MrSaints@users.noreply.github.com>
2016-05-20 18:51:55 +0100

    Move scripts import to separate file

---
+++ scripts.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+	_ "github.com/claudebot/hippie/scripts/excuses"
+	_ "github.com/claudebot/hippie/scripts/giphy"
+	_ "github.com/claudebot/hippie/scripts/httpcodes"
+	_ "github.com/claudebot/hippie/scripts/ping"
+)
